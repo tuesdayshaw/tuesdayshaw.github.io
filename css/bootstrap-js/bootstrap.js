b3bf82b3c166d4d12ad454dbe8b858e1b3c9cc4d
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-04-09 20:48:35 -0500

    - Updating Responsiveness and playing with layout

--- css/bootstrap-js/bootstrap.js
+++ css/bootstrap-js/bootstrap.js
@@ -546,7 +546,7 @@ var Carousel = function ($) {
   var ARROW_RIGHT_KEYCODE = 39; // KeyboardEvent.which value for right arrow key
 
   var Default = {
-    interval: 5000,
+    interval: 7000,
     keyboard: true,
     slide: false,
     pause: 'hover',
