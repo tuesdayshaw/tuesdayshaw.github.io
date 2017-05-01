75b0e193c57c9e8a8bd4c1acd8223ad04236bdbe
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-01 09:43:04 -0500

    - Updating Responsiveness and playing with layout

---
+++ js/welcome.js
@@ -0,0 +1,22 @@
+/**
+ * Created by canidmars on 4/30/17.
+ */
+
+
+
+$(document).ready(function(){
+    "use strict";
+
+
+    // $('.rose').animate({
+    //     backgroundColor: {
+    //         val: '#FFF',
+    //         easing: 'linear',
+    //         direction: 'alternate',
+    //         duration: 2000
+    //     }
+    //
+    // })
+});
+
+
